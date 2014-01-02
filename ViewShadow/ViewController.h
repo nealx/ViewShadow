16c35cee25728211d81c570b4b0a56a527c99f3d
Nealx <nealxue.sh@gmail.com>
2014-01-02 09:28:37 +0800

    Initial Commit

---
+++ ViewShadow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ViewShadow
+//
+//  Created by Nealx on 14-1-2.
+//  Copyright (c) 2014年 www.creacree.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ViewShadow/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ViewShadow
+//
+//  Created by Nealx on 14-1-2.
+//  Copyright (c) 2014年 www.creacree.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
